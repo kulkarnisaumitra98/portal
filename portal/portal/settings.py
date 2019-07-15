0bb77d6e97cca8372e8d1c746ff7983e59308f25
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-07-15 19:28:06 +0000

    debug false

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&0-m1kly0fg1dv*f@el1u-*^86y58vz&$!_luo*==(ocg^6cnd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['clash2k19.pythonanywhere.com']
 
